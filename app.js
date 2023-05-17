3b1e35d371aa74d06f143ba4098d733222499837
Gattaku <kazuyoshi.ogata.ab@gmail.com>
2023-05-17 23:01:08 +0900

    DB情報GET機能実装完了

--- app.js
+++ app.js
@@ -1,84 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const XLSX = require('xlsx');
-const fs = require('fs');
-const getCurrentTime = require("./components/getDate"); //現在時刻を取ってくる関数をimport
-const crypto = require("crypto");
-
-
+const dbRoute = require("./routes/db");
 const app = express();
-app.use(express.static("./public"));
+
+app.use(express.static("./public")); //ローカルサーバー接続時に最初に接続する静的HTMLファイル
 app.use(express.json());
 const port = 3000;
-const excelFilePath = 'data.xlsx';
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// フロントエンドからの文字列を取得するAPIのルートエンドポイント
-const apiEndpoint = '/addString';
-
-
-
-
-// エクセルファイルに文字列を追記するAPI
-app.post(apiEndpoint, (req, res) => {
-    const { userName, text } = req.body;
-    const currentTime = getCurrentTime.getCurrentTime();
-
-    // 追加をつかさどる部分 
-    const colIndex = "ABCD"; //どの列までするか
-    const id = crypto.randomUUID;
-    const colContent = [currentTime, userName, id, text] //中身の配列
-
-
-
-    // エクセルファイルの読み込み
-    let workbook;
-    if (fs.existsSync(excelFilePath)) {
-        workbook = XLSX.readFile(excelFilePath);
-    } else {
-        workbook = XLSX.utils.book_new();
-    }
-
-    // 最初のシートを選択
-    const sheetName = workbook.SheetNames[0];
-    const worksheet = workbook.Sheets[sheetName];
-    let cnt = 0;
-    let rowIndex = 1;
-    let checkRow = `A${rowIndex}`;
-    let cell = worksheet[checkRow];
-    while (cnt < 1) {
-        const checkFlg = !cell || cell.v === "";
-        if (checkFlg) {
-            console.log(checkRow);
-            console.log(worksheet[`A${rowIndex - 1}`].v);
-            cnt++;
-        } else {
-            rowIndex++;
-            checkRow = `A${rowIndex}`;
-            cell = worksheet[checkRow];
-        }
-    }
-
-
-
-    // 変更可能範囲指定
-    const rangeCol = `${colIndex[colIndex.length - 1]}${rowIndex}`;
-    const range = worksheet['!ref'] = `A1:${rangeCol}`;
-
-    for (let i = 0; i < colIndex.length; i++) {
-        worksheet[`${colIndex[i]}${rowIndex}`] = { t: 's', v: colContent[i] };
-    }
-
-
-
-    // エクセルファイルを保存
-    XLSX.writeFile(workbook, excelFilePath);
-
-    res.redirect("/");
-    // res.status(200).json({ message: '文字列が追加されました。' });
-});
+// フロントエンドからエクセルDBにアクセスるAPIのルートエンドポイント
+app.use("/api/v1/excel-data-base", dbRoute);
 
 // サーバーを起動
 app.listen(port, () => {
--- public/index.js
+++ public/index.js
@@ -1,22 +1,17 @@
-// $sendButton = document.getElementById("sendButton");
-
-// $sendButton.addEventListener("submit", () => {
-//     alert(document.myform.userName.value);
-//     document.myform.action = "/addString";
-//     document.myform.submit();
-// })
 
 
 $form = document.getElementById("myform");
 $userName = document.getElementById("user");
 $text = document.getElementById("textContent");
+$getButton = document.getElementById("getbutton");
+$currentDB = document.getElementById("currentDB");
 
 const createNewDB = async (userName, text) => {
     try {
-        await axios.post("/addString", {
+        await axios.post("/api/v1/excel-data-base", {
             userName: userName,
             text: text,
-            // id: Math.random().toString(),
+            // id: Math.random().toString(), //inputタグ以外の情報も追加できることを確認済み
         });
         window.alert("DBへの登録が完了しました!");
         $userName.value = "";
@@ -26,12 +21,44 @@ const createNewDB = async (userName, text) => {
     }
 }
 
-
-
 $form.addEventListener("submit", (e) => {
     e.preventDefault();
     createNewDB($userName.value, $text.value);
 })
 
+const getInnerHtml = (arg) => {
+    const allData = arg.map((rowData) => {
+        const eachRow = rowData.map((elm) => {
+            return `<th>${elm}</th>`
+        });
+        const eachRowText = eachRow.join("");
+        const nodeText = `<tr>
+                            ${eachRowText}
+                        </tr>`;
+        return nodeText;
+    })
+    return allData.join("");
+}
 
-console.log(axios);
+const getDatabase = async () => {
+    try {
+        console.log("getボタンは押されました。")
+        const getDatabase = await axios.get("/api/v1/excel-data-base")
+        // window.alert("DBの情報を取得しました");
+        const getData = getDatabase.data.database;
+        const eachData = getData.map((elm) => {
+            return elm.map((elm2) => elm2.v);
+        });
+        console.log(eachData);
+        const innerNode = getInnerHtml(eachData);
+        $currentDB.innerHTML = `<table>
+                                ${innerNode}
+                                </table>`;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+$getButton.addEventListener("click", (e) => {
+    getDatabase();
+})
